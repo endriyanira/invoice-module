ba915160efc14d5f63014fcc27bd28ee4aee6864
Endriyani Rahayu <endriyani.rahayu@ui.ac.id>
2024-08-01 14:20:10 +0700

    added invoice items table

--- client/src/components/AddInvoice.js
+++ client/src/components/AddInvoice.js
@@ -7,6 +7,16 @@ import {
   Card,
   CardContent,
   Grid,
+  InputAdornment,
+  Box,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  CardMedia,
 } from "@mui/material";
 import { DemoItem } from "@mui/x-date-pickers/internals/demo";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
@@ -26,29 +36,64 @@ const AddInvoice = () => {
     notes: "",
     items: [],
   });
-  const [items, setItems] = useState([]);
+
+  const [items, setItems] = useState([
+    { name: "", picture: "", quantity: 1, price: 0, totalPrice: 0 },
+  ]);
+
   const [newItem, setNewItem] = useState({
     name: "",
     quantity: 1,
     price: 0,
+    totalPrice: 0,
   });
 
   //   Handle change for customer name, salesperson, payment type, notes
-  const handleInputChange = (e) => {
-    setInvoice({ ...invoice, [e.target.name]: e.target.value });
+  const handleInputChange = (e, field) => {
+    setInvoice({ ...invoice, [field]: e.target.value });
   };
 
-  const handleProductSelect = (e, value) => {
-    setInvoice({ ...invoice, items: [...invoice.products, value] });
+  const handleChangeItemProduct = (e, value, index) => {
+    if (value) {
+      const name = value.name;
+      const picture = value.picture;
+      const price = value.price;
+      const updatedItems = [...items];
+      updatedItems[index].name = name;
+      updatedItems[index].picture = picture;
+      updatedItems[index].price = price;
+      updatedItems[index].totalPrice =
+        parseFloat(price) * parseFloat(updatedItems[index].quantity);
+      setItems(updatedItems);
+    }
+  };
+
+  const handleChangeQuantity = (e, index) => {
+    const updatedItems = [...items];
+    updatedItems[index].quantity = e.target.value;
+    updatedItems[index].totalPrice =
+      parseFloat(updatedItems[index].price) *
+      parseFloat(updatedItems[index].quantity);
+    setItems(updatedItems);
   };
 
   const handleChangeItem = (e, field) => {
-    setNewItem({ ...newItem, [field]: e.target.value });
+    setNewItem({
+      ...newItem,
+      [field]: e.target.value,
+    });
+
+    if (field === "quantity") {
+      const price = newItem.price;
+      const qty = newItem.quantity;
+      console.log(price);
+      console.log(qty);
+    }
   };
 
   const handleAddItem = () => {
+    // concat
     setItems([...items, newItem]);
-    setNewItem({ name: "", quantity: 1, price: 0 });
   };
 
   const handleRemoveItem = (index) => {
@@ -78,91 +123,238 @@ const AddInvoice = () => {
   };
 
   return (
-    <Card variant="outlined" sx={{ maxWidth: 500 }}>
+    <Card variant="outlined">
       <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Invoice
+        <Typography gutterBottom paddingBottom={2} variant="h5" component="div">
+          Invoice Form
         </Typography>
         <form onSubmit={handleSubmit}>
-          {/* Date */}
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DemoItem label="Responsive variant">
-              <DatePicker
-                label="Date"
-                defaultValue={dayjs(invoice.date)}
-                onChange={(newValue) =>
-                  setInvoice({ ...invoice, date: newValue })
-                }
-                required
-              />
-            </DemoItem>
-          </LocalizationProvider>
+          <Box sx={{ display: "flex", flexDirection: "column", gap: "20px" }}>
+            {/* Date */}
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DemoItem>
+                <DatePicker
+                  label="Date"
+                  defaultValue={dayjs(invoice.date)}
+                  onChange={(newValue) =>
+                    setInvoice({ ...invoice, date: newValue })
+                  }
+                  required
+                />
+              </DemoItem>
+            </LocalizationProvider>
 
-          {/* Customer Name */}
-          <TextField
-            label="Customer Name"
-            name="customerName"
-            value={invoice.customerName}
-            onChange={handleInputChange}
-            required
-          />
-
-          {/* Salesperson Name */}
-          <TextField
-            label="Salesperson Name"
-            name="salespersonName"
-            value={invoice.salespersonName}
-            onChange={handleInputChange}
-          />
-
-          {/* Notes */}
-          <TextField
-            label="Notes"
-            name="notes"
-            value={invoice.notes}
-            onChange={handleInputChange}
-          />
-
-          {/* Payment Type */}
-          <TextField
-            label="Payment Type"
-            name="paymentType"
-            value={invoice.paymentType}
-            onChange={handleInputChange}
-          />
-
-          <Grid container spacing={3}>
-            <Grid item xs={12}>
-              <Typography variant="h6">Add Invoice Items</Typography>
-            </Grid>
-          </Grid>
-          <Grid item xs={12}>
-            <Autocomplete
-              id="product-input"
-              options={products}
-              getOptionLabel={(option) => option.name}
-              onChange={handleProductSelect}
-              renderInput={(params) => (
-                <TextField {...params} label="Product" variant="outlined" />
-              )}
+            {/* Customer Name */}
+            <TextField
+              label="Customer Name"
+              name="customerName"
+              value={invoice.customerName}
+              onChange={(e) => handleInputChange(e, "customerName")}
+              required
             />
-            {/* <TextField
-              fullWidth
-              label="Item"
-              value={newItem.name}
-                onChange={(event) => handleChange(event, "name")}
-            /> */}
+
+            {/* Salesperson Name */}
+            <TextField
+              label="Salesperson Name"
+              name="salespersonName"
+              value={invoice.salespersonName}
+              onChange={(e) => handleInputChange(e, "salespersonName")}
+            />
+
+            {/* Payment Type */}
+            <TextField
+              label="Payment Type"
+              name="paymentType"
+              value={invoice.paymentType}
+              onChange={(e) => handleInputChange(e, "paymentType")}
+              required
+            />
+
+            {/* Notes */}
+            <TextField
+              label="Notes"
+              name="notes"
+              value={invoice.notes}
+              multiline
+              rows={4}
+              onChange={(e) => handleInputChange(e, "notes")}
+            />
+          </Box>
+
+          <Box marginTop={3} borderRadius={1}>
+            <TableContainer component={Paper}>
+              <Table aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="left">Items</TableCell>
+                    <TableCell align="left">Price</TableCell>
+                    <TableCell align="left">Qty</TableCell>
+                    <TableCell align="left">Total</TableCell>
+                    <TableCell align="left">Action</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {items.map((row, index) => (
+                    <TableRow
+                      key={index.toString()}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        <Grid item xs={3}>
+                          <Autocomplete
+                            id="product-input"
+                            options={products}
+                            getOptionLabel={(option) => option.name}
+                            onChange={(e, value) =>
+                              handleChangeItemProduct(e, value, index)
+                            }
+                            renderInput={(params) => (
+                              <TextField
+                                {...params}
+                                label="Product"
+                                variant="outlined"
+                              />
+                            )}
+                          />
+                        </Grid>
+                      </TableCell>
+
+                      {/* Price */}
+                      <TableCell align="right">
+                        <Grid item xs={3}>
+                          <TextField
+                            fullWidth
+                            disabled
+                            type="text"
+                            value={row.price}
+                            onChange={(event) =>
+                              handleChangeItem(event, "price")
+                            }
+                          />
+                        </Grid>
+                      </TableCell>
+
+                      {/* Quantity */}
+                      <TableCell align="right">
+                        <Grid item xs={3}>
+                          <TextField
+                            fullWidth
+                            type="number"
+                            value={row.quantity}
+                            onChange={(event) =>
+                              handleChangeQuantity(event, index)
+                            }
+                          />
+                        </Grid>
+                      </TableCell>
+                      <TableCell align="left">
+                        Rp{" "}
+                        {row.totalPrice
+                          ? parseFloat(row.totalPrice).toFixed(2)
+                          : "0"}
+                      </TableCell>
+                      <TableCell>
+                        <Button
+                          variant="outlined"
+                          color="error"
+                          onClick={() => handleRemoveItem(index)}
+                        >
+                          Remove
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+            {/* <Grid container spacing={2}>
+              <Grid item xs={3}>
+                <Autocomplete
+                  id="product-input"
+                  options={products}
+                  getOptionLabel={(option) => option.name}
+                  onChange={handleChangeItemProduct}
+                  renderInput={(params) => (
+                    <TextField {...params} label="Product" variant="outlined" />
+                  )}
+                />
+              </Grid>
+
+              <Grid item xs={3}>
+                <TextField
+                  fullWidth
+                  type="text"
+                  value={newItem.quantity}
+                  onChange={(event) => handleChangeItem(event, "quantity")}
+                />
+              </Grid>
+
+              <Grid item xs={3}>
+                <TextField
+                  fullWidth
+                  type="number"
+                  value={newItem.totalPrice}
+                  onChange={(event) => handleChangeItem(event, "totalPrice")}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">Rp</InputAdornment>
+                    ),
+                  }}
+                />
+              </Grid>
+              <Grid item xs={3}>
+                <TextField
+                  fullWidth
+                  //   label="Qty"
+                  type="text"
+                  value={newItem.quantity}
+                  onChange={(event) => handleChangeItem(event, "quantity")}
+                />
+              </Grid>
+            </Grid> */}
+            {/* </Box> */}
+          </Box>
+          <Grid item xs={3} paddingY={2}>
+            <Button variant="contained" onClick={handleAddItem}>
+              Add Item
+            </Button>
           </Grid>
-          <Autocomplete
-            id="product-input"
-            options={products}
-            getOptionLabel={(option) => option.name}
-            onChange={handleProductSelect}
-            renderInput={(params) => (
-              <TextField {...params} label="Product" variant="outlined" />
-            )}
-          />
-          <Button type="submit">Add Invoice</Button>
+
+          {/* {items.length !== 0 &&
+            items.map((item, index) => (
+              <Grid item xs={12} key={index}>
+                <Grid container spacing={1} alignItems="center">
+                  <Grid item xs={3}>
+                    <Typography variant="body1">{item.name}</Typography>
+                  </Grid>
+                  <Grid item xs={1}>
+                    <Typography variant="body1">{item.quantity}</Typography>
+                  </Grid>
+                  <Grid item xs={2}>
+                    <Typography variant="body1">${item.totalPrice}</Typography>
+                  </Grid>
+                  <Grid item xs={2}>
+                    <Typography variant="body1">
+                      $
+                      {(
+                        parseFloat(item.quantity) * parseFloat(item.price)
+                      ).toFixed(2)}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={2}>
+                    <Button
+                      variant="outlined"
+                      color="error"
+                      onClick={() => handleRemoveItem(index)}
+                    >
+                      Remove
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Grid>
+            ))} */}
+
+          <Button type="submit">Create Invoice</Button>
         </form>
       </CardContent>
     </Card>
