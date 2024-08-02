0a8e2688ca06fe4d1045ff57a680ee67fe81008b
Endriyani Rahayu <endriyani.rahayu@ui.ac.id>
2024-08-02 21:48:07 +0700

    feat(fe): updated navbar so the logo is clicable

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
 
 const Navbar = () => {
   return (
@@ -18,9 +18,17 @@ const Navbar = () => {
             color="inherit"
             aria-label="menu"
           ></IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Wida Tech
-          </Typography>
+
+          <Button variant="text" href="/">
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1 }}
+              color={"white"}
+            >
+              Wida Tech
+            </Typography>
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
