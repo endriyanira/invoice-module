eb1e38c48b912131989a36bd32e1e8babfa0d7de
Endriyani Rahayu <endriyani.rahayu@ui.ac.id>
2024-08-02 21:45:25 +0700

    feat(fe): added getDate utils for show date format on invoice card

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,3 +1,9 @@
+export const getDate = (isoString) => {
+  const date = new Date(isoString);
+  const formattedDate = date.toDateString().split(" ").slice(1, 4).join(" ");
+  return formattedDate;
+};
+
 export const getDateFromISOString = (isoString) => {
   const date = new Date(isoString);
 
@@ -13,4 +19,11 @@ export const formatCurrency = (amount, currency = "IDR", locale = "id-ID") => {
     style: "currency",
     currency: currency,
   }).format(amount);
+};
+
+export const isEmptyobject = (obj) => {
+  if (obj === null || obj === undefined) {
+    return true; // Handle null and undefined cases
+  }
+  return Object.keys(obj).length === 0;
 };
